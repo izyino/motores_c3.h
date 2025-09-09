1f225ee29176e1ad58cfa95e9e1fb4b6182eb897
izyino <info@izyino.com.br>
2025-09-09 16:49:14 -0300

    Update exemplo1.ino

--- examples/exemplo1/exemplo1.ino
+++ examples/exemplo1/exemplo1.ino
@@ -12,7 +12,7 @@
 
 #include <motores_c3.h>
 
-motores_c3 x(2);                         //0 indica motor(es) DC em CN1 
+motores_c3 x(4);                         //0 indica motor(es) DC em CN1 
                                          //1,2,3 ou 4=indica motor de passo em CN1
 
 bool sent=false;
@@ -28,9 +28,12 @@ void loop() {
   if (x.xtipostep==0){                    //se há motor(es) DC conectado(s)
     x.beep(2, 200, 2000, 100);            //emite 2 beep de 200ms cada, 2000Hz, intervalo entre eles de 100ms 
     x.led(20, 100, 50);                   //pisca o LED 20 vezes com 100ms aceso e 50ms apagado
-    x.runDC(0, 2500, 100, 1);             //ativa motor DC 0 por 2,5s, vel 100%
-    x.runDC(1, 5000, 75, 0);              //ativa motor DC 1 por 5s, vel 50%
-
+    if (x.xtime[0]==0){                   //se o motor DC n.0 estiver parado
+      x.runDC(0, 2500, 100, 1);           //ativa motor DC 0 por 2,5s, vel 100%
+    }                                     //
+    if (x.xtime[1]==0){                   //se o motor DC n.1 estiver parado
+      x.runDC(1, 5000, 75, 0);            //ativa motor DC 1 por 5s, vel 50%
+    }                                     //
   }  
 
   if (x.xtipostep!=0){                    //se há motor de passo conectado
@@ -38,11 +41,11 @@ void loop() {
       x.beep(2, 200, 2000, 100);          //emite 2 beep de 200ms cada, 2000Hz, intervalo entre eles de 100ms 
       x.led(20, 100, 50);                 //pisca o LED 20 vezes com 100ms aceso e 50ms apagado
       sent=!sent;                         //inverte o sentido, voltas entre 1 e 10
-      voltas=random(1, 11)*2048;          //velocidade entre 2 e 12 RPM, e começa a girar
-      x.runStep(voltas, random(2,13), sent);   
+      voltas=random(1, 11)*200;           //velocidade entre 1 e 60 RPM, e começa a girar
+      x.runStep(voltas, random(1,61), sent);   
     }  
   }
    
-  x.setms(6000);
-  while(x.getms()>0){}                    //apenas exemplo de espera não blocante (6 segundos)
+  x.setms(1000);
+  while(x.getms()>0){}                    //apenas exemplo de espera não blocante (1 segundos)
 }
